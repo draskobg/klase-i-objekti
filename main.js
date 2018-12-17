c87ea55d9ae15942a18dafe5dee795ae87243be8
Drasko <draskosorak@gmail.com>
2018-12-17 19:47:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+/*
+let brojac = 1600000330
+class Klijent {                                                         // Ovde definisemo klasu.  Tj kako ce izgledati unos dole u const
+    constructor(ime,stanje) {
+        this.ime = ime
+        this.brojRacuna = ++brojac
+        this.stanje = stanje
+    }
+
+    setIme(ime) {
+        if (ime.length < 5) {
+            console.log("Nema dovoljno slova");
+            
+        }
+        else {
+            this.ime = ime
+        }
+       
+    }
+
+
+    prenetiSredstva(primalac, iznos) {
+        if (this.stanje - iznos >= 0) {                                  // Ukoliko je stanje vece od 0                  
+            primalac.stanje += iznos;                               // tom klijentu povecati
+            this.stanje -= iznos;                                // sebi da smanji sredstva
+            console.log("Trenutno stanje je", this.stanje);
+        }                                                     // Napisi ovo 
+        else console.log("Nemate dovoljno sredstava");       // U suprotnom napisi ovo
+    }
+}
+const petar = new Klijent("Petar Petrovic",2000)                   // Unosimo atrubute kao sto je zadato u klasi na pocetku
+const ana = new Klijent("Ana Pavlovic",15000)                      // Unosimo atrubute kao sto je zadato u klasi na pocetku
+const ljubica = new Klijent("Ljubica Dikic",4000)                  // Unosimo atrubute kao sto je zadato u klasi na pocetku
+
+*/
+
+let energija = 100
+class Igrac {
+    constructor (ime, energija) {
+        this.ime = ime
+        this.energija = energija
+    }
+    umorIgraca(ime, vrednost)
+   { if (this.energija - vrednost > 0) {
+        this.energija += vrednost;
+        ime.energija -= vrednost;
+        console.log("Vasa energija je", this.energija);
+    
+    }
+    else console.log("Nemate vise energije");}
+}
+
+
+const petar = new Igrac("Petar Petrovic", 80)
+const ana = new Igrac("Ana Ruzicic", 90)
+
+
